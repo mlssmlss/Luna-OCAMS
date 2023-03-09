dde622a86748d8373a3c84d6c8235939d7ba453b
mlssmlss <91305206+mlssmlss@users.noreply.github.com>
2023-03-09 15:17:50 +0800

    first commit

---
+++ luna/src/test/java/org/ssglobal/revalida/codes/LunaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ssglobal.revalida.codes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
