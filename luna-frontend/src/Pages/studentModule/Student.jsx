0a4f280edd1c6cff23e9c8688a01a66a3eb79204
Nicolas <78528181+NicolasLibre@users.noreply.github.com>
2023-03-11 00:58:42 +0800

    modifying Student.jsx and Student.css

--- luna-frontend/src/Pages/studentModule/Student.jsx
+++ luna-frontend/src/Pages/studentModule/Student.jsx
@@ -1,122 +1,80 @@
 import React from "react";
 import { TextField } from "@mui/material";
+import Footer from "../../Components/Footer/Footer";
+import "./Student.css";
 
 const Student = () => {
   return (
-    <div className="form-input">
-      <div className="studentNumber">
-        <TextField
-          name="studNumber"
-          type="text"
-          label="Student Number"
-          color="success"
-          size="small"
-          variant="outlined"
-          sx={{
-            width: { sm: 200, md: 388 },
-
-            mb: 2,
-          }}
-          required
+    <div className="studentContainer">
+      <div className="hero-reg">
+        <video
+          className="reg-video"
+          src="/videos/video-1.mp4"
+          autoPlay
+          loop
+          muted
         />
       </div>
 
-      <div className="studentProgram">
-        <TextField
-          name="program"
-          color="success"
-          size="small"
-          variant="outlined"
-          type="text"
-          label="Program"
-          sx={{
-            width: { sm: 200, md: 388 },
-
-            mb: 2,
-          }}
-          required
-        />
-      </div>
-
-      <div className="uNamePhoneContainer">
-        <div className="userName">
-          <TextField
-            name="username"
-            type="text"
-            label="Password"
-            color="success"
-            size="small"
-            variant="outlined"
-            sx={{
-              width: { sm: 200, md: 388 },
-
-              mb: 2,
-            }}
-            required
-          />
-        </div>
-
-        <div className="phone">
-          <TextField
-            name="phoneNumber"
-            type="text"
-            label="First Name"
-            color="success"
-            size="small"
-            variant="outlined"
-            sx={{
-              width: { sm: 200, md: 388 },
-
-              mb: 2,
-            }}
-          />
-        </div>
-      </div>
-
-      <div>
-        <TextField
-          name="email"
-          type="text"
-          label="Middle Name"
-          fullWidth
-          variant="outlined"
-          color="success"
-          size="small"
-          sx={{
-            width: { sm: 200, md: 388 },
-
-            mb: 2,
-          }}
-          InputLabelProps={{ style: { fontSize: 15 } }}
-          required
-        />
-      </div>
-
-      <div className="brgyCityContainer">
-        <div className="brgy">
-          <TextField
-            name="brgy"
-            type="text"
-            label="Last Name"
-            fullWidth
-            color="success"
-            size="small"
-            variant="outlined"
-            sx={{
-              width: { sm: 200, md: 388 },
-
-              mb: 2,
-            }}
-            required
-          />
-        </div>
-
-        <div className="bDateContainer">
-          <div className="bDateInput">
+      <div className="form-input">
+        <div className="student-info">
+          <h3>Student Information</h3>
+          <div className="student-input">
             <TextField
-              name="dot"
-              type="date"
-              label="Birthdate"
+              className="stdNo"
+              name="studNumber"
+              type="text"
+              label="Student Number"
+              color="success"
+              size="small"
+              variant="outlined"
+              sx={{
+                width: { sm: 200, md: 388 },
+
+                mb: 2,
+              }}
+              required
+            />
+
+            <TextField
+              className="fName"
+              name="firstName"
+              type="text"
+              label="First Name"
+              color="success"
+              size="small"
+              variant="outlined"
+              sx={{
+                width: { sm: 200, md: 388 },
+
+                mb: 2,
+              }}
+              required
+            />
+
+            <TextField
+              className="mName"
+              name="middleName"
+              type="text"
+              label="Middle Name"
+              fullWidth
+              variant="outlined"
+              color="success"
+              size="small"
+              sx={{
+                width: { sm: 200, md: 388 },
+
+                mb: 2,
+              }}
+              required
+            />
+
+            <TextField
+              className="lName"
+              name="lastName"
+              type="text"
+              label="Last Name"
+              fullWidth
               color="success"
               size="small"
               variant="outlined"
@@ -129,115 +87,8 @@ const Student = () => {
             />
           </div>
         </div>
-
-        <div className="city">
-          <TextField
-            name="city"
-            type="text"
-            label="Semester"
-            color="success"
-            size="small"
-            variant="outlined"
-            sx={{
-              width: { sm: 200, md: 388 },
-
-              mb: 2,
-            }}
-            required
-          />
-        </div>
       </div>
-
-      <div className="provinceBDateContainer">
-        <div className="province">
-          <TextField
-            name="province"
-            type="text"
-            label="Year Level"
-            color="success"
-            size="small"
-            variant="outlined"
-            sx={{
-              width: { sm: 200, md: 388 },
-
-              mb: 2,
-            }}
-            required
-          />
-        </div>
-      </div>
-      <div className="provinceBDateContainer">
-        <div className="province">
-          <TextField
-            name="province"
-            type="text"
-            label="Academic Level"
-            color="success"
-            size="small"
-            variant="outlined"
-            sx={{
-              width: { sm: 200, md: 388 },
-
-              mb: 2,
-            }}
-            required
-          />
-        </div>
-      </div>
-      <div className="provinceBDateContainer">
-        <div className="province">
-          <TextField
-            name="province"
-            type="text"
-            label="Status"
-            color="success"
-            size="small"
-            variant="outlined"
-            sx={{
-              width: { sm: 200, md: 388 },
-
-              mb: 2,
-            }}
-            required
-          />
-        </div>
-      </div>
-      <div className="provinceBDateContainer">
-        <div className="province">
-          <TextField
-            name="province"
-            type="text"
-            label="Subject"
-            color="success"
-            size="small"
-            variant="outlined"
-            sx={{
-              width: { sm: 200, md: 388 },
-
-              mb: 2,
-            }}
-            required
-          />
-        </div>
-      </div>
-      <div className="provinceBDateContainer">
-        <div className="province">
-          <TextField
-            name="province"
-            type="text"
-            label="Grades"
-            color="success"
-            size="small"
-            variant="outlined"
-            sx={{
-              width: { sm: 200, md: 388 },
-
-              mb: 2,
-            }}
-            required
-          />
-        </div>
-      </div>
+      <Footer />
     </div>
   );
 };
